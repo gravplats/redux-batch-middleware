f9cbf080fbf77b801fda164f2ffb66b324817a5f
Pavlos Polianidis <ppoliani@gmail.com>
2017-04-24 16:05:50 +0100

    Consider non FSA actions
    In case we have a non flux standard action, we'd like to give the processing responsibility to other middlewares.
    For example, if the array contains a thunk we'd like redux thunk to deal with it.

--- modules/index.js
+++ modules/index.js
@@ -1,12 +1,11 @@
 export const type = '@@redux-batch-middleware/BATCH';
 
-export const batch = ({ dispatch }) => {
-    return (next) => (action) => {
-        return Array.isArray(action)
-            ? dispatch({ type: type, payload: action })
-            : next(action);
-    };
-};
+export const batch = ({ dispatch }) => (next) => (action) =>
+  Array.isArray(action)
+    ? action.every(a => typeof a !== 'function')
+      ? dispatch({ type: type, payload: action })
+      : action.filter(Boolean).map(dispatch)
+    : next(action);
 
 export const batching = (reducer) => {
     return function batcher(state, action) {
